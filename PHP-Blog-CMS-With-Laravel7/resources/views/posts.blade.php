0833b11c2a040d3d852f69ea563b8dad692d4534
k0zyX <barisdogan2k@gmail.com>
2020-10-02 15:19:12 +0300

    Homepage Cards added

--- PHP-Blog-CMS-With-Laravel7/resources/views/posts.blade.php
+++ PHP-Blog-CMS-With-Laravel7/resources/views/posts.blade.php
@@ -1,19 +1,27 @@
     @include('header')
     <div class="container-fluid row">
-        <div class="card col" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
+        <div class="card col-3 mr-4 ml-4 mt-4" style="width: 18rem;">
+            <img src="#" class="card-img-top">
             <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <a href="#" class="btn btn-primary">Read Post</a>
             </div>
         </div>
-        <div class="card col" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
+        <div class="card col-3 mr-4 ml-4 mt-4" style="width: 18rem;">
+            <img src="#" class="card-img-top">
             <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <a href="#" class="btn btn-primary">Read Post</a>
+            </div>
+        </div>
+        <div class="card col-3 mr-4 ml-4 mt-4" style="width: 18rem;">
+            <img src="#" class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Read Post</a>
             </div>
         </div>
     </div>
