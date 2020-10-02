818feec6f13ee6693ed62af6ae749e3e19b7ba24
k0zyX <barisdogan2k@gmail.com>
2020-10-02 15:19:35 +0300

    card images removed

--- PHP-Blog-CMS-With-Laravel7/resources/views/posts.blade.php
+++ PHP-Blog-CMS-With-Laravel7/resources/views/posts.blade.php
@@ -1,7 +1,6 @@
     @include('header')
     <div class="container-fluid row">
         <div class="card col-3 mr-4 ml-4 mt-4" style="width: 18rem;">
-            <img src="#" class="card-img-top">
             <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -9,7 +8,6 @@
             </div>
         </div>
         <div class="card col-3 mr-4 ml-4 mt-4" style="width: 18rem;">
-            <img src="#" class="card-img-top">
             <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -17,7 +15,6 @@
             </div>
         </div>
         <div class="card col-3 mr-4 ml-4 mt-4" style="width: 18rem;">
-            <img src="#" class="card-img-top">
             <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
